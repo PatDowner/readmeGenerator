ba047fefa8d588d95aaf68c93ffeb784208e05b3
Pat Downer <patricia.downer@gmail.com>
2020-08-12 10:34:20 -0700

    added text for what writes to the readme file

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# readmeGenerator
+# Title: a
+  ![badge](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+  -------
+  
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Description:
+  
+  b
+  
+
+  ## Installation:
+  
+  c
+  
+
+  ## Usage:
+  
+  d
+  
+
+  ## Contributing:
+  
+  f
+  
+
+  ## Testing:
+  
+  g
+  
+
+  ## License:
+  
+  MIT
+  
+
+  ## Questions:
+  
+  Github Username: [h](https://github.com/h)
+  
+  Contact me via email at: [pat.downer85@gmail.com](mailto:pat.downer85@gmail.com)
+  
--- index.js
+++ index.js
@@ -1,62 +1,123 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 
-const question = () => {
-  inquirer
-    .prompt([
-      {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        message: 'Describe your project.'
-      },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'Describe how to install your project.'
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'How should this project be used?'
-      },
-      {
-        type: 'list',
-        name: 'license',
-        message: 'Please choose a license type.'
-      choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
-      },
-      {
-        type: 'input',
-        name: 'contributing',
-        message: 'Who contributed to this project?'
-      },
-      {
-        type: 'input',
-        name: 'testing',
-        message: 'What testing was completed for this project?'
-      },
-      {
-        type: 'input',
-        name: 'questions',
-        message: 'List any questions you want to include in the readme.'
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?'
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?'
-      }
+// const question = () => {
+inquirer
+  // list of questions to gather information for the readme
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Describe your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Describe how to install your project.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How should this project be used?'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose a license type.',
+      choices: ['MIT', 'APACHE_2.0', 'GPL_3.0', 'BSD_3', 'None']
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: 'Who contributed to this project?'
+    },
+    {
+      type: 'input',
+      name: 'testing',
+      message: 'What testing was completed for this project?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your GitHub username?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email?'
+    }
+    // end of question list
+  ])
+  .then(res => {
+
+    fs.writeFile('README.md', `# Title: ${res.title}
+  ![badge](https://img.shields.io/badge/license-${res.license}-blue.svg)
+  
+  -------
+  
+  ## Table of Contents
+
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Description:
+  
+  ${res.description}
+  
+
+  ## Installation:
+  
+  ${res.installation}
+  
+
+  ## Usage:
+  
+  ${res.usage}
+  
+
+  ## Contributing:
+  
+  ${res.contributing}
+  
+
+  ## Testing:
+  
+  ${res.testing}
+  
+
+  ## License:
+  
+  ${res.license}
+  
+
+  ## Questions:
+  
+  Github Username: [${res.github}](https://github.com/${res.github})
+  
+  Contact me via email at: [${res.email}](mailto:${res.email})
+  `, (err) => {
+      if (err) { console.log(err) }
+      console.log('File Created!')
+    })
 
 
 
-    ])
-}
+
+
+
+
+    // end of .then
+  })
+  .catch(err => console.log(err))
+// }
